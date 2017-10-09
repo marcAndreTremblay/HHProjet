8a242dcabe21dab7e4092667ac564e01ecfa920e
marcAndreTremblay <marct549@gmail.com>
2017-10-09 11:34:42 -0400

    as

--- bd_sql_cmd.sql
+++ bd_sql_cmd.sql
@@ -1,3 +1,20 @@
-INSERT INTO `hqdb`.`participant` 
-(`Participant_id`, `First_Name`, `Last_Name`, `Street_Name`, `Street_Number`, `Telehpone`, `Email`, `Montagne_id`, `Participation_cout`)
- VALUES ('1', 'First_Name', 'Last_Name', 'Street_Name', 0, 'tel', 'email',1, 1);
+INSERT INTO `hqdb`.`participant`
+(`Participant_id`,
+`First_Name`,
+`Last_Name`,
+`Street_Name`,
+`Street_Number`,
+`Telehpone`,
+`Email`,
+`Montagne_id`,
+`Participation_cout`)
+VALUES
+(<{Participant_id: }>,
+<{First_Name: }>,
+<{Last_Name: }>,
+<{Street_Name: }>,
+<{Street_Number: 0}>,
+<{Telehpone: }>,
+<{Email: }>,
+<{Montagne_id: 0}>,
+<{Participation_cout: 1}>);
