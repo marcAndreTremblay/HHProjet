943bb7e7f76e61b00db6585d3c85c243da5aafa2
marcAndreTremblay <marct549@gmail.com>
2017-10-09 11:32:15 -0400

    test

---
+++ bd_sql_cmd.sql
@@ -0,0 +1,3 @@
+INSERT INTO `hqdb`.`participant` 
+(`Participant_id`, `First_Name`, `Last_Name`, `Street_Name`, `Street_Number`, `Telehpone`, `Email`, `Montagne_id`, `Participation_cout`)
+ VALUES ('1', 'First_Name', 'Last_Name', 'Street_Name', 0, 'tel', 'email',1, 1);
